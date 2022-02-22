73c0e8e28c63c23a804462f6c6b48912657327b4
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-02-22 20:32:51 +0100

    Update nav-bar.component.ts

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public router: Router,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialogModule
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toProfile(): void {
+    this.router.navigate(['users']);
   }
 
+  toMovies(): void {
+    this.router.navigate(['movies']);
+  }
+
+  logOut(): void {
+    localStorage.clear();
+    this.snackBar.open('You have been successfully logged out', 'Ok', {
+      duration: 2000,
+    });
+    this.router.navigate(['welcome']);
+  }
 }
