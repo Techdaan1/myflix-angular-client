8a2791393c8873296db107c674a0e57cbbce2b38
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-02-22 08:29:31 +0100

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -13,7 +13,7 @@ const apiUrl = 'https://myflix-application-2021.herokuapp.com/';
 @Injectable({
   providedIn: 'root',
 })
-export class UserRegistrationService {
+export class FetchApiDataService {
   // Inject the HttpClient module to the constructor params
   // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {}
